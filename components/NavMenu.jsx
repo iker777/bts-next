dad5ed2cbbe3b86fc124401e698bd7c1af92d207
“iker777” <ikeralvarez777@gmail.com>
2026-03-17 10:04:08 +0100

    Menu de navegación

---
+++ components/NavMenu.jsx
@@ -0,0 +1,33 @@
+"use client"
+import { useState } from "react"
+
+export default function NavMenu() {
+    const [estaAbierto, setEstaAbierto] = useState(false)
+
+    return (
+        <header className="bg-fondo py-5 px-10 flex justify-between items-center h-[15vh]">
+            <a href="index.html">
+                <img className="w-24" src="img/logo.jpg" alt="logo de BTS" />
+            </a>
+            <div onClick={() => setEstaAbierto(!estaAbierto)} className="w-8 flex flex-col gap-2 lg:hidden group">
+                <div className="rounded w-full h-1 bg-white group-hover:bg-azul group-active:bg-rosa rotate-45"></div>
+                <div className="rounded w-full h-1 bg-white hidden"></div>
+                <div className="rounded w-full h-1 bg-white group-hover:bg-azul group-active:bg-rosa -rotate-45"></div>
+            </div>
+            <nav className="hidden lg:flex">
+                <ul className="text-white flex gap-4">
+                    <li><a className="hover:text-morado active:text-rosa" href="#">Próximas fechas</a></li>
+                    <li><a className="hover:text-morado active:text-rosa" href="#">Comprar entradas</a></li>
+                    <li><a className="hover:text-morado active:text-rosa" href="#">Tienda</a></li>
+                </ul>
+            </nav>
+            <nav className={("flex lg:hidden bg-amber-600 absolute w-full min-h-[85vh] top-[15vh] left-0 justify-around items-center ") + (estaAbierto ? ("translate-x-0") : ("translate-x-full"))}>
+                <ul className="text-white items-center gap-4 flex flex-col">
+                    <li><a className="hover:text-morado active:text-rosa" href="#">Próximas fechas</a></li>
+                    <li><a className="hover:text-morado active:text-rosa" href="#">Comprar entradas</a></li>
+                    <li><a className="hover:text-morado active:text-rosa" href="#">Tienda</a></li>
+                </ul>
+            </nav>
+        </header >
+    )
+}
